2c3e7a24f57170760eebff16c2e43489e6ccc92a
nsaars <mimalliktv04@gmai.com>
2024-07-13 23:16:11 +0500

    .

--- app/handlers/start_handlers.py
+++ app/handlers/start_handlers.py
@@ -10,4 +10,4 @@ async def start_handler(message: types.Message, state: FSMContext):
     from_user = message.from_user
     create_user(from_user.id, from_user.username, from_user.full_name)
     await message.answer("Наш менеджер уже получил заявку и скоро свяжется с Вами!")
-    await message.bot.send_message(ADMIN, f"Новая заявка от {from_user.username} ({from_user.full_name}).")
+    await message.bot.send_message(ADMIN, f"Новая заявка от @{from_user.username} ({from_user.full_name}).")
